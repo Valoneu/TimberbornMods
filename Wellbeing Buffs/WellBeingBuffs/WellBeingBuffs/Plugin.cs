0c66a907ab097c2eb7e0dfe61bc50c8f5ce86243
Valoneu <filip@becicka.cz>
2022-10-15 21:03:26 +0200

    lol

---
+++ Wellbeing Buffs/WellBeingBuffs/WellBeingBuffs/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using TimberApi.ModSystem;
+using TimberApi.ConsoleSystem;
+using TimberApi.ConfigSystem;
+
+namespace WellBeingBuffs
+{
+    [HarmonyPatch]
+    public class WellBeingBuffs : IModEntrypoint
+    {
+        public static WellBeingBuffConfig Config;
+
+        public void Entry(IMod mod, IConsoleWriter consoleWriter)
+        {
+            Config = mod.Configs.Get<WellBeingBuffConfig>();
+            var harmony = new Harmony("Valoneu.plugins.WellBeingBuff");
+            harmony.PatchAll();
+        }
+    }
+    public class WellBeingBuffConfig : IConfig
+    {
+        public string ConfigFileName => "WellBeingBuffs";
+
+        public int WellBeingMultipler = 2;
+    }
+}
---
+++ Wellbeing Buffs/WellBeingBuffs/WellBeingBuffs/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace WellBeingBuffs
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "WellBeingBuffs";
+        public const string PLUGIN_NAME = "WellBeingBuffs";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
